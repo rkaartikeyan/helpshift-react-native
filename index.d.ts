4ff447fe212c91c940bc9ab49f0eaa6998747f5e
Karthikeyan R <karthikeyan.r2@photoninfotech.net>
2022-05-10 15:19:35 +0530

    Fix: Type Declaration issue

--- index.d.ts
+++ index.d.ts
@@ -1,32 +1,37 @@
-interface userWithEmail {
-  identifier?: string
-  email: string,
-  name: string,
-  authToken?: string
+declare namespace Helpshift {
+  interface userWithEmail {
+    identifier?: string
+    email: string,
+    name: string,
+    authToken?: string
+  }
+
+  interface userWithoutEmail {
+    identifier: string
+    email?: string,
+    name: string,
+    authToken?: string
+  }
+
+  export type user = userWithEmail | userWithoutEmail
+
+  export interface cifs {
+    [key: string]: string[]
+  }
+
+  export interface Module {
+    init: (apiKey: string, domain: string, appId: string) => void;
+    login: (user: user) => void;
+    logout: (user: user) => void;
+    showConversation: () => void;
+    showFAQs: () => void;
+    showFAQById: ({ id }: {id: number}) => void;
+    showSectionById: ({ id }: {id: number}) => void;
+    showConversationWithCIFs: (cifs: cifs) => void;
+    showFAQsWithCIFs: (cifs: cifs) => void;
+    requestUnreadMessagesCount: () => Awaited<Promise<number>>
+  }
 }
 
-interface userWithoutEmail {
-  identifier: string
-  email?: string,
-  name: string,
-  authToken?: string
-}
-
-interface cifs {
-  [key: string]: string[]
-}
-
-interface Helpshift {
-  init: (apiKey: string, domain: string, appId: string) => void;
-  login: (user: userWithEmail | userWithoutEmail) => void;
-  logout: (user: userWithEmail | userWithoutEmail) => void;
-  showConversation: () => void;
-  showFAQs: () => void;
-  showFAQById: ({ id }: {id: number}) => void;
-  showSectionById: ({ id }: {id: number}) => void;
-  showConversationWithCIFs: (cifs: cifs) => void;
-  showFAQsWithCIFs: (cifs: cifs) => void;
-  requestUnreadMessagesCount: () => Awaited<Promise<number>>
-}
-
-export default Helpshift;
+declare const module: Helpshift.Module;
+export default module;
